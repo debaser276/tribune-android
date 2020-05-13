0e8c3c36a6bf2fbcf9c8ba31625140c60ae75d71
debaser <debaser86@yandex.ru>
2020-05-13 18:34:04 +0300

    AuthViewModel

--- app/src/main/java/ru/debaser/projects/tribune/view/AuthFragment.kt
+++ app/src/main/java/ru/debaser/projects/tribune/view/AuthFragment.kt
@@ -1,30 +1,23 @@
 package ru.debaser.projects.tribune.view
 
-import android.content.Context
-import android.content.SharedPreferences
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.edit
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_auth.*
-import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
 import org.koin.core.parameter.parametersOf
-import org.koin.core.qualifier.named
-import retrofit2.Response
 import ru.debaser.projects.tribune.*
-import ru.debaser.projects.tribune.repository.Me
-import ru.debaser.projects.tribune.repository.Repository
 import ru.debaser.projects.tribune.utils.*
-import java.io.IOException
+import ru.debaser.projects.tribune.viewmodel.AuthViewModel
+import androidx.lifecycle.observe
 
 class AuthFragment : Fragment() {
-    private val sharedPref: SharedPreferences by inject(named(API_SHARED_FILE))
-    private val repository: Repository by inject()
+    private val authViewModel: AuthViewModel by inject()
     private val dialog: LoadingDialog by inject { parametersOf(requireActivity()) }
 
     override fun onCreateView(
@@ -32,7 +25,7 @@ class AuthFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        if (isAuthenticated()) {
+        if (authViewModel.isAuthenticated()) {
             findNavController().navigate(AuthFragmentDirections.actionAuthFragmentToIdeasFragment())
         }
         return inflater.inflate(R.layout.fragment_auth, container, false)
@@ -42,56 +35,50 @@ class AuthFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         loginBtn.setOnClickListener {
-            if (!isValid(passwordEt.text.toString())) {
-                passwordTil.error = getString(R.string.password_incorrect)
-            } else {
-                viewLifecycleOwner.lifecycleScope.launch {
-                    dialog.apply {
-                        setTitle(R.string.authentication)
-                        show()
-                    }
-                    try {
-                        val response =
-                            repository.authenticate(
-                                loginEt.text.toString(),
-                                passwordEt.text.toString()
-                            )
-                        if (response.isSuccessful) {
-                            setUserAuth(response)
-                            findNavController().navigate(AuthFragmentDirections.actionAuthFragmentToIdeasFragment())
-                        } else {
-                            toast(R.string.authentication_failed)
-                        }
-                    } catch (e: IOException) {
-                        toast(R.string.error_occurred)
-                    } finally {
-                        dialog.dismiss()
-                    }
-                }
-            }
+            authViewModel.authenticate(
+                loginEt.text.toString(),
+                passwordEt.text.toString()
+            )
         }
 
         noAccountTv.setOnClickListener {
             findNavController().navigate(AuthFragmentDirections.actionAuthFragmentToRegFragment())
+        }
+
+        passwordEt.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {}
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                passwordTil.error = null
+            }
+        })
+
+        with (authViewModel) {
+            passwordErrorEvent.observe(viewLifecycleOwner) {
+                passwordTil.error = getString(R.string.password_incorrect)
+            }
+            showLoadingDialogEvent.observe(viewLifecycleOwner) {
+                showLoadingDialog(it)
+            }
+            moveToIdeasFragmentEvent.observe(viewLifecycleOwner) {
+                findNavController().navigate(AuthFragmentDirections.actionAuthFragmentToIdeasFragment())
+            }
+            showToastEvent.observe(viewLifecycleOwner) {
+                toast(it)
+            }
+        }
+    }
+
+    private fun showLoadingDialog(show: Boolean) {
+        if (show) {
+            dialog.show()
+        } else {
+            dialog.dismiss()
         }
     }
 
     override fun onStop() {
         super.onStop()
         hideKeyboard()
-    }
-
-    private fun isAuthenticated() =
-        sharedPref.getString(AUTHENTICATED_SHARED_TOKEN, "")?.isNotEmpty() ?: false
-
-    private fun setUserAuth(response: Response<Me>) {
-        sharedPref.edit {
-            putLong(AUTHENTICATED_SHARED_ID, response.body()!!.id)
-            putString(AUTHENTICATED_SHARED_USERNAME, response.body()!!.username)
-            putString(AUTHENTICATED_SHARED_TOKEN, response.body()!!.token)
-            putBoolean(AUTHENTICATED_SHARED_ISHATER, response.body()!!.isHater)
-            putBoolean(AUTHENTICATED_SHARED_ISPROMOTER, response.body()!!.isPromoter)
-            putBoolean(AUTHENTICATED_SHARED_ISREADER, response.body()!!.isReader)
-        }
     }
 }
---
+++ app/src/main/java/ru/debaser/projects/tribune/viewmodel/AuthViewModel.kt
@@ -0,0 +1,77 @@
+package ru.debaser.projects.tribune.viewmodel
+
+import android.content.SharedPreferences
+import androidx.core.content.edit
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import org.koin.android.ext.android.inject
+import org.koin.core.qualifier.named
+import retrofit2.Response
+import ru.debaser.projects.tribune.R
+import ru.debaser.projects.tribune.repository.Me
+import ru.debaser.projects.tribune.repository.Repository
+import ru.debaser.projects.tribune.utils.*
+import java.io.IOException
+
+class AuthViewModel(
+    private val repository: Repository,
+    private val sharedPref: SharedPreferences
+) : ViewModel() {
+
+    private val _passwordErrorEvent = MutableLiveData<Boolean>()
+    val passwordErrorEvent: LiveData<Boolean>
+        get() = _passwordErrorEvent
+
+    private val _showLoadingDialogEvent = MutableLiveData<Boolean>()
+    val showLoadingDialogEvent: LiveData<Boolean>
+        get() = _showLoadingDialogEvent
+
+    private val _moveToIdeasFragmentEvent = MutableLiveData<Boolean>()
+    val moveToIdeasFragmentEvent: LiveData<Boolean>
+        get() = _moveToIdeasFragmentEvent
+
+    private val _showToastEvent = SingleLiveEvent<Int>()
+    val showToastEvent: LiveData<Int>
+        get() = _showToastEvent
+
+    fun authenticate(login: String, password: String) {
+        if (!isValid(password)) {
+            _passwordErrorEvent.value = true
+        } else {
+            viewModelScope.launch {
+                _showLoadingDialogEvent.value = true
+                try {
+                    val response =
+                        repository.authenticate(login, password)
+                    if (response.isSuccessful) {
+                        setUserAuth(response)
+                        _moveToIdeasFragmentEvent.value = true
+                    } else {
+                        _showToastEvent.value = R.string.authentication_failed
+                    }
+                } catch (e: IOException) {
+                    _showToastEvent.value = R.string.error_occurred
+                } finally {
+                    _showLoadingDialogEvent.value = false
+                }
+            }
+        }
+    }
+
+    fun isAuthenticated() =
+        sharedPref.getString(AUTHENTICATED_SHARED_TOKEN, "")?.isNotEmpty() ?: false
+
+    private fun setUserAuth(response: Response<Me>) {
+        sharedPref.edit {
+            putLong(AUTHENTICATED_SHARED_ID, response.body()!!.id)
+            putString(AUTHENTICATED_SHARED_USERNAME, response.body()!!.username)
+            putString(AUTHENTICATED_SHARED_TOKEN, response.body()!!.token)
+            putBoolean(AUTHENTICATED_SHARED_ISHATER, response.body()!!.isHater)
+            putBoolean(AUTHENTICATED_SHARED_ISPROMOTER, response.body()!!.isPromoter)
+            putBoolean(AUTHENTICATED_SHARED_ISREADER, response.body()!!.isReader)
+        }
+    }
+}
